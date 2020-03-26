1b676fac2d7d6f6acfdf0fe1996297e0f8e4c276
shmy <pandahao123>
2020-03-26 09:27:10 +0800

    test ci

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,4 @@ docker build -t shmy/sso:latest .
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker push shmy/sso:latest
 sudo apt-get update && sudo apt-get install sshpass -y
-ls -a
-sudo sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no root@47.75.55.94 "cd /home/sso && ./start.sh"
+sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no root@47.75.55.94 "cd /home/sso && ./start.sh"
