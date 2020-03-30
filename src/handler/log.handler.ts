6f5b71182f62ba8e2d95b568955c1c1456b86f07
shmy <pandahao123>
2020-03-30 19:04:48 +0800

    修复人员应用显示

--- src/handler/log.handler.ts
+++ src/handler/log.handler.ts
@@ -1,5 +1,4 @@
-import {mapList} from "./basic.handler";
-import LogModel, {getLogModelRepository} from "../model/log.model";
+import {getLogModelRepository} from "../model/log.model";
 import {Context} from "koa";
 import parsePaging from "../util/paging.util";
 
@@ -7,8 +6,9 @@ export const listLog = async (ctx: Context) => {
   const {offset, limit} = parsePaging(ctx);
   const repo = getLogModelRepository();
   const sql = "SELECT `log`.`*`, `app`.`name` AS `app_name`, `people`.`username` AS `personnel_username`, `people`.`real_name` AS `personnel_real_name` FROM `log` LEFT JOIN `personnel` AS `people` ON `people`.`id` = `log`.`personnel_id` LEFT JOIN `application` AS `app` ON `app`.`id` = `log`.`application_id` ORDER BY `log`.`created_at` DESC LIMIT " + offset + ", " + limit;
-  const countSql = "SELECT COUNT(`log`.`id`) AS `cnt` FROM `log`";
+  const countSql = "SELECT COUNT(`log`.`id`) AS `count` FROM `log`";
   const result = await repo.query(sql);
   const count = await repo.query(countSql);
-  ctx.success({result: result, count: count.cnt});
+  // TODO: count.cnt is undefined on production
+  ctx.success({result: result, ...count});
 };
